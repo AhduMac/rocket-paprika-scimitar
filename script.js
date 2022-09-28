7dd4416217338871cae1c1134d497de4a0897466
remotedonut <andmac92@gmail.com>
2022-09-28 11:04:43 -0400

    Started from scratch with new JS file

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-let playerScore = 0
-let computerScore = 0
-
+let playerScore = 0;
+let computerScore = 0;
+let result = "";
+let playerSelection;
+let compSelection = "";
 const handSignal = ['rocket', 'paprika', 'scimitar']
 
+function playerChoice(playerSelection){
+    let playerSign = prompt("Please type: rocket, paprika, or scimitar");
+    
+            if (playerSign.toLowerCase() === 'rocket'){
+         playerSelection = 'rocket';
+         return playerSelection; 
+    }    
+    else if (playerSign.toLowerCase() === 'paprika'){
+        return playerSelection = 'paprika'
+    } 
 
-function computerChoice(){
+    else if (playerSign.toLowerCase() === 'scimitar'){
+        return playerSelection = 'scimitar'
+    }
+    else (console.log('fix ya typo, dungus'));
+    }
+ console.log(playerChoice())  ; 
+console.log(playerSign)
+
+
+/*function computerChoice(){
     compThrow = Math.floor(Math.random() * handSignal.length)
-    return handSignal[compThrow]
+    return compSelection = handSignal[compThrow];
 }
 
-function playerChoice(){
-    prompt("Please type: rocket, paprika, or scimitar")
-    return
-}
+console.log(computerChoice());
 
 function playRound(playerSelection, computerSelection) {
-    let result = ""
+    
 
-    if  ((playerChoice == 'rocket' && computerChoice == 'scimitar') ||
-        (playerChoice == 'scimitar' && computerChoice == 'paprika') || 
-        (playerChoice == 'paprika' && computerChoice == 'rocket')) {
+        if  ((playerSelection == 'rocket' && computerSelection == 'scimitar') ||
+            (playerSelection == 'scimitar' && computerSelection == 'paprika') || 
+            (playerSelection == 'paprika' && computerSelection == 'rocket')) {
 
-        playerScore += 1
-        result = ("You did it! " + playerChoice + " beats " + computerChoice + "You have " + playerScore + " points to the computer's " + computerScore + "points.")
+            playerScore++
+             console.log("You did it! " + playerSelection + " beats " + computerSelection + "You have " + playerScore + " points to the computer's " + computerScore + "points.");
 
-        if playerScore == 5
-            result += "You have won the whole dang thing!"
+            if (playerScore == 5) 
+             console.log("You have won the whole dang thing!")
 
-    }
+        }
 
-    else if playerChoice == computerChoice
-            result = "You absolute fool. You have tied." 
+       else if (playerSelection == computerSelection)
+             console.log("You absolute fool. You have tied."); 
     
     
-    else if ((playerChoice == 'rocket' && computerChoice == 'paprika' ) ||
-            (playerChoice == 'scimitar' && computerChoice == 'rocket' ) || 
-            (playerChoice == 'paprika' && computerChoice == 'scimitar')) {
+        else if ((playerSelection == 'rocket' && computerSelection == 'paprika' ) ||
+                (playerSelection == 'scimitar' && computerSelection == 'rocket' ) || 
+                (playerSelection == 'paprika' && computerSelection == 'scimitar')) {
+             console.log("Gosh dang it, Human! " + computerSelection + " beats " + playerSelection + "You have " + playerScore + " points to the computer's " + computerScore + "points.");       
+             computerScore++;
+             
 
-             computerScore += 1
-             result = ("Gosh dang it, Human! " + computerChoice + " beats " + playerChoice + "You have " + playerScore + " points to the computer's " + computerScore + "points.")
-
-    if computerScore == 5
-        result += "You have lost the whole dang thing! You coward!"
-    }    
+        if (computerScore == 5) 
+             console.log("You have lost the whole dang thing! You coward!");
+        
+        }  
+         
 }
 
-console.log(computerChoice())
+
+console.log(playRound(playerSelection, compSelection));
+console.log('Your score is: ' + playerScore);
+console.log("Your Opponent's score is: " + computerScore);  
+
+*/
+
+
+
+
---
+++ scroban.js
@@ -0,0 +1,15 @@
+//Generating the "throw" or  handsignal thrown by the user
+//let userSign = prompt("Please type: rocket, paprika, or scimitar");
+//console.log(userSign + 'dooder');
+
+
+//Generating the "throw" or  handsignal thrown by the computer
+let compSign = Math.floor(Math.random()*3);
+let signal = ['rocket', 'paprika', 'scimitar'];
+let compThrow = signal[compSign];
+
+
+
+
+
+
